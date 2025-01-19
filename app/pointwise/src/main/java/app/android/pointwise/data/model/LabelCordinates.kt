f79a0488065730994799ddcb26fcd83e254ad1e8
Akhil Krishnan <akhilrk19@gmail.com>
2025-01-19 16:01:07 +0530

    kdoc

--- app/pointwise/src/main/java/app/android/pointwise/data/model/LabelCordinates.kt
+++ app/pointwise/src/main/java/app/android/pointwise/data/model/LabelCordinates.kt
@@ -1,7 +1,14 @@
 package app.android.pointwise.data.model
 
 import androidx.compose.ui.graphics.Color
-
+/**
+* Data class for Label cordinates
+ * @param x x axis
+ * @param y y axis
+ * @param color Color of the label
+ * @param showPointIntersection Enable/Disable point intersection
+ * @param dotRadius Radius of the circle
+*/
 data class LabelCordinates(
     val x: Float,
     val y: Float,
